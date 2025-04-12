d5437182e330748e4436bf6b9e9422698681e435
Prayag Sagar <prayag260583@gmail.com>
2025-04-12 19:46:52 +0530

    Full project upload

--- server/server.js
+++ server/server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
+app.use(express.static('../client'));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public')); // folder for HTML, CSS, JS
+app.use(express.static(path.join(__dirname, 'public')));
+ // folder for HTML, CSS, JS
 
 // MongoDB connection
 mongoose.connect('mongodb://localhost:27017/vastusuraksha', {
@@ -16,12 +19,36 @@ mongoose.connect('mongodb://localhost:27017/vastusuraksha', {
   .catch(err => console.log(err));
 
 // Example route
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/dashboard', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
+  });
+
+  app.get('/analytics', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'analytics.html'));
+  });
+
+  app.get('/reports', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'reports.html'));
+  });
+
+  app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+  });
+  app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'register.html'));
+  });
+  app.get('/data-input', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'data-input.html'));
+  });
+  app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+  });
+
 // Start server
-const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
